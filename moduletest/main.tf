2783ea826f046f71b3448a692cf221943d632069
Harkar-Dinesh <harkardinesh707@gmail.com>
2026-05-13 23:57:39 +0530

    values

---
+++ moduletest/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+  ami = var.ami_id
+  instance_type = var.type
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name1
+}
---
+++ moduletest/variable.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  description = "passing values to main.tf"
+  type = string
+  default = ""
+}
+
+variable "type" {
+  description = "passing values to main.tf"
+  type = string
+  default = ""
+}
+
+variable "bucket_name1" {
+  type = string
+  default = ""
+
+}
